744c3a3e4d34eae401f267297b45717841ef1994
jfspps <jfsapps@gmail.com>
2021-02-06 21:42:35 +0000

    first commit

---
+++ src/main/java/com/jfspps/reactiveapps/ReactiveAppsApplication.java
@@ -0,0 +1,13 @@
+package com.jfspps.reactiveapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveAppsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveAppsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jfspps/reactiveapps/ReactiveAppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfspps.reactiveapps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveAppsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
